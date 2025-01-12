befeed34da4c85bbe4f0739cf9b1f0c18c805413
JamalColaire <jamal8colaire@gmail.com>
2025-01-11 23:34:59 -0500

    switched string

--- obj/Debug/net9.0/CSharp-Workings.AssemblyInfo.cs
+++ obj/Debug/net9.0/CSharp-Workings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp-Workings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533bf8759f6348bf96705f55b47e1adeb15deae0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3358161d03dcb213b75bbcd5c249200c2f908a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp-Workings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp-Workings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
